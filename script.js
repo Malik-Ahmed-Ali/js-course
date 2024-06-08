47070eb1598e926ba89b324fc870ce0a3d9a3bcb
Ahmed Ali <malikahmedaliawan6@gmail.com>
2024-06-08 13:24:29 +0500

    html file script file mutation type cohersion

---
+++ script.js
@@ -0,0 +1,30 @@
+var firstName = "Ahmed"; // Declaratio + initialization
+var lastName; // Declaration
+console.log(firstName);
+var age = 24;
+console.log(age);
+var fullAge = true;
+console.log(fullAge);
+var job;
+console.log(job);
+job = "Businessman";
+console.log(job);
+
+// JS var dynamically detectes value data type
+// Type corecion (converting one type to another)
+// like in example below converting number into string to make a bigger string
+console.log("My Name is ", firstName, " and my age is ", age);
+
+// variable mutation (changing one type to another or changine value)
+age = "twenty two";
+console.log("My Name is " + firstName + " and my age is " + age);
+
+// Data Types number, string, boolean, undefined, null
+
+/*
+multi line comment
+*/
+
+lastName=prompt('Enter you last name');
+
+console.log(firstName, lastName);
